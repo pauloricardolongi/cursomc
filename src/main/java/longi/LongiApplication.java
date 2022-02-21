60489cd492187436cc9aa87f74902841e4a45859
pauloricardolongi <pauloricardolong@gmail.com>
2022-02-21 13:54:36 -0300

    projeto criado

---
+++ src/main/java/longi/LongiApplication.java
@@ -0,0 +1,13 @@
+package longi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LongiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/longi/LongiApplicationTests.java
@@ -0,0 +1,13 @@
+package longi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
